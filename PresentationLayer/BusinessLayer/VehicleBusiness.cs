7e998b81e8a6159b5bbc040828d62c0e8cb97d9a
Lazar Jovancic <lazarjovancic9@gmail.com>
2021-01-09 17:03:37 +0100

    Kreirana klasa VehicleBusiness

---
+++ PresentationLayer/BusinessLayer/VehicleBusiness.cs
@@ -0,0 +1,63 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLayer
+{
+    public class VehicleBusiness
+    {
+
+        private readonly VehicleRepository vehicleRepository;
+
+        public VehicleBusiness()
+        {
+            this.vehicleRepository = new VehicleRepository();
+        }
+
+        public List<Vehicle> GetAllVehicles()
+        {
+            return this.vehicleRepository.GetAllVehicles();
+        }
+        
+
+        public List<Vehicle> GetAvailableVehicles() 
+        {
+            return this.vehicleRepository.GetAllVehicles().Where(v => v.Condition == "U garazi").ToList();
+        }
+
+        public bool InsertVehicle(Vehicle v)
+        {
+            if (this.vehicleRepository.InsertVehicle(v) > 0) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateVehicle(Vehicle v)
+        {
+            if (this.vehicleRepository.UpdateVehicle(v) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeleteVehicle(int Id)
+        {
+            if (this.vehicleRepository.DeleteVehicle(Id) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
