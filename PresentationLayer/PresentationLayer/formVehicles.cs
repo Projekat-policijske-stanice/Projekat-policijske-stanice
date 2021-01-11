de75f0974eda25176ada84a78ec6291eaa25c187
Nikola Garovic <9-2018@ftn.edu.rs>
2021-01-11 21:33:04 +0100

    Klasa za rad sa Prekrsajima(Offenses)

--- PresentationLayer/PresentationLayer/formVehicles.cs
+++ PresentationLayer/PresentationLayer/formVehicles.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Policijska_stanica
+namespace PresentationLayer
 {
     public partial class formVehicles : Form
     {
